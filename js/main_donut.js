3cf875ef7d22c5dfec5824f32126149f756daa7a
OpenGov <OpenGov_Admin@Delphis-MacBook-Pro.local>
2015-06-06 00:09:11 -0700

    add dashed canvas lines -> drops framerate

--- js/main_donut.js
+++ js/main_donut.js
@@ -129,6 +129,70 @@ function analyze(budget, target_year){
 
 }
 
+//for canvas stuff..
+function custom(selection) {
+  selection.each(function() {
+    var root = this;
+    var canvas = root.parentNode.appendChild(document.createElement("canvas"));
+    var context = canvas.getContext("2d");
+
+    canvas.style.position = "absolute";
+    canvas.style.top = root.offsetTop + "px";
+    canvas.style.left = root.offsetLeft + "px";
+    canvas.style['z-index'] = -1;
+
+    d3.timer(redraw);
+
+    // Clear the canvas and then iterate over child elements.
+    function redraw() {
+      canvas.width = root.getAttribute("width");
+      canvas.height = root.getAttribute("height");
+      for (var child = root.firstChild; child; child = child.nextSibling) draw(child);
+    }
+
+  	//for dashed lines
+		function calcPointsCirc( cx,cy, rad, dashLength){
+			var n = rad/dashLength,
+			  alpha = Math.PI * 2 / n,
+			  pointObj = {},
+			  points = [],
+			  i = -1;
+			while( i < n ){
+			  var theta = alpha * i,
+			      theta2 = alpha * (i+1);
+			  
+			  points.push({ //why if dont multiple by 1 it break?
+			  	x : (Math.cos(theta) * rad) + 1*cx, 
+			  	y : (Math.sin(theta) * rad) + 1*cy, 
+			  	ex : (Math.cos(theta2) * rad) + 1*cx, 
+			  	ey : (Math.sin(theta2) * rad) + 1*cy}
+			  );
+				i+=2;
+			}              
+			return points;            
+		} 
+
+
+    function draw(elem) { //how to assign a class attribute to this???
+      switch (elem.tagName.split("-")[0]) { //sketched way to assign classes/ID's....
+        case "circle": {
+          context.strokeStyle = elem.getAttribute("strokeStyle");
+          context.beginPath();
+          var pointArray = calcPointsCirc(elem.getAttribute("x"),elem.getAttribute("y"),elem.getAttribute("r"), 1);
+          for(p = 0; p < pointArray.length; p++){
+		        context.moveTo(pointArray[p].x, pointArray[p].y);
+		        context.lineTo(pointArray[p].ex, pointArray[p].ey);
+		    	}
+          //context.arc(elem.getAttribute("x"), elem.getAttribute("y"), elem.getAttribute("r"), 0, 2 * Math.PI);
+				  //context.lineWidth = 1;
+          context.stroke();
+          break;
+        }
+      }
+    }
+  });
+};
+
 function generateDonut(refData){
 	var divH = parseInt( d3.select("#chartArea").style("height") );
 	var divW = parseInt( d3.select("#chartArea").style("width") );
@@ -196,68 +260,11 @@ function generateDonut(refData){
 	    //.attr("transform", "translate(" + w / 2 + "," + (h) / 2 + ")");
 	//How to select the item below for redrawing?
 
-function custom(selection) {
-  selection.each(function() {
-    var root = this;
-    var canvas = root.parentNode.appendChild(document.createElement("canvas"));
-    var context = canvas.getContext("2d");
-
-    canvas.style.position = "absolute";
-    canvas.style.top = root.offsetTop + "px";
-    canvas.style.left = root.offsetLeft + "px";
-    canvas.style['z-index'] = -1;
-
-    d3.timer(redraw);
-
-    // Clear the canvas and then iterate over child elements.
-    function redraw() {
-      canvas.width = root.getAttribute("width");
-      canvas.height = root.getAttribute("height");
-      for (var child = root.firstChild; child; child = child.nextSibling) draw(child);
-    }
-
-    function draw(element) { //how to assign a class attribute to this???
-      switch (element.tagName.split("-")[0]) { //sketched way to assign classes/ID's....
-        case "circle": {
-          context.strokeStyle = element.getAttribute("strokeStyle");
-          context.beginPath();
-          context.arc(element.getAttribute("x"), element.getAttribute("y"), element.getAttribute("radius"), 0, 2 * Math.PI);
-				  context.lineWidth = 1;
-          context.stroke();
-          break;
-        }
-      }
-    }
-  });
-};
-
 	var svg = d3.select("#chartArea").append("svg")
 	    .attr("width", w)
 	    .attr("height", h)
 	  .append("g")
 	    .attr("transform", "translate(" + w / 2 + "," + (h) / 2 + ")");
-
-	//text: svg, canvas, foreignObject
-	/*svg.append("text")
-		.attr("id", "valueOutput")
-		.attr("text-anchor", "middle")
-		.attr("transform","translate(" + innerRadius/20 + "," + (innerRadius/24 - 12) + ")");
-	d3.select('#valueOutput').html("Hover to View");
-	svg.append("text")
-		.attr("id", "valueSource")
-		.attr("text-anchor", "middle")
-		.attr("transform","translate(" + innerRadius/20 + "," + (innerRadius/24 + 12) + ")");
-	d3.select('#valueSource').html("Total Budget: " + formatNumber(d3.sum(budget)));*/
-
-	var valueText = {h: 100, w: 400};
-	svg.append("foreignObject")
-		.attr("x", -valueText.w/2)
-		//.attr("y", -valueText.h/2)
-		.attr("width", valueText.w)
-		.attr("height", valueText.w)
-		.classed("valueText", true)
-		.append("xhtml:body").append("xhtml:p")
-		.html("Hover To View <br/>" + "Total Budget: " + formatNumber(d3.sum(budget)));
 
 
 	//onhover comparison circle
@@ -269,7 +276,7 @@ function custom(selection) {
 	sketch.append("custom:circle")
     .attr("x", w/2)
     .attr("y", h/2)
-    .attr("radius", 0)
+    .attr("r", 0)
     .attr("strokeStyle", "orange")
 
 
@@ -295,6 +302,33 @@ function custom(selection) {
 	    	lines[1].push([alpha, l - 5])
 	    	lines[2].push([alpha, l - 10])
 	    	lines[3].push([alpha, l - 2])
+
+
+	    	l = innerRadius + rad(refData[d.data.id]['budget']);
+    		var x0 = l * Math.sin(alpha);
+				var y0 = l * Math.cos(alpha);
+				var newAngle = alpha + Math.PI/2;
+				var newl = l * Math.PI / Object.keys(refData).length;
+				var x2 = x0 + newl * Math.sin(newAngle)
+				var y2 = y0 + newl * Math.cos(newAngle)
+				var x1 = x0 - newl * Math.sin(newAngle)
+				var y1 = y0 - newl * Math.cos(newAngle)
+
+				svg.append("line")
+			    .style("stroke-width", "2px")
+			    .style("stroke", 'black')
+			    .classed('a' + d.data.id + ' budget', true)
+			    .attr("x1", x1)
+			    .attr("x2", x2)
+			    .attr("y1", -y1)
+			    .attr("y2", -y2)
+			    .on("mouseover", function(){
+			    	d3.selectAll('.budget').style("stroke-width", "5px")
+			    	d3.select("#aggregate").style("font-weight", "bold")
+			    })
+			    .on("mouseout", function(){
+			    	d3.selectAll('.budget').style("stroke-width", "2.5px")
+			    })
 	    })
 	    .attr("d", arc)
 	    .classed('arcs', true)
@@ -329,11 +363,10 @@ function custom(selection) {
 					    .attr("x2", x2)
 					    .attr("y1", -y1)
 					    .attr("y2", -y2)
+					    //Need to add action listener so that if this is clicked same behaviour as bar...
+					    //can pass in a method that will do it..
 					}
 	    	}
-
-	    	//How to have a mix colour if arc is highlighted (by maroon) and mouseover?
-	    	//Dashed lines or zebra stripes
 
 	    	/*d3.select("#valueOutput").html(refData[d.data.id]['agency']);
 	      d3.select("#valueSource").html(
@@ -342,10 +375,10 @@ function custom(selection) {
 	      	'<tspan x="1" dy="15">Budget: $' + formatNumber(refData[d.data.id]['budget']) + '</tspan>'
 	      	);*/
 	      d3.select(".valueText").html( //turn this into a table
-	      	refData[d.data.id]['agency'] + '<br>' + 
-	      	'Budget Positive: $' + formatNumber(refData[d.data.id]['budget_positive']) + '<br>' + 
-	      	'Budget Negative: $' + formatNumber(refData[d.data.id]['budget_negative']) + '<br>' + 
-	      	'Budget: $' + formatNumber(refData[d.data.id]['budget']) + '<br>'
+	      	'<b>' + refData[d.data.id]['agency'] + '</b><br>' + '<table align="center">' + 
+	      	'<tr><td align="center">' + 'Budget Positive:' + '</td><td align="center">$' + formatNumber(refData[d.data.id]['budget_positive']) + '</td></tr>' + 
+	      	'<tr><td align="center">' + 'Budget Negative:' + '</td><td align="center">$' + formatNumber(refData[d.data.id]['budget_negative']) + '</td></tr>' + 
+	      	'<tr id="aggregate"><td align="center">' + 'Budget:' + '</td><td align="center">$' + formatNumber(refData[d.data.id]['budget']) + '</td></tr>' + '<table'
 	      )
 
 	      //animation for .distance-circle to change position over time
@@ -361,7 +394,7 @@ function custom(selection) {
 		    	.transition()
 		    	.delay(100)
 		      .duration(500)
-		      .attr("radius", rad(refData[d.data.id]['budget']) + innerRadius)
+		      .attr("r", rad(refData[d.data.id]['budget']) + innerRadius)
 	    })
 	    .on("mouseout", function(d){
 	    	if(d.data.circle == true){
@@ -420,11 +453,11 @@ function custom(selection) {
 					sketch.append("custom:circle-" + id) //canvas comparison circle
 				    .attr("x", w/2)
 				    .attr("y", h/2)
-				    .attr("radius", smallestDim)
+				    .attr("r", smallestDim)
 				    .attr("strokeStyle", "maroon")
 				    .transition()
 				    .duration(time)
-				    .attr("radius", d.outerRadius)
+				    .attr("r", d.outerRadius)
 
 					console.log(d)
 					console.log(d3.select('#' + id))
@@ -436,17 +469,41 @@ function custom(selection) {
 						.duration(time*2)
 						.attr("r", smallestDim)
 						.remove(); 	*/
+
+					//if click on the dashed bar -> should also remove the bars
 					d3.select('circle-' + id) //canvas comparison circle
 						.transition()
 						.delay(100)
 						.duration(time*2)
-						.attr("radius", smallestDim)
+						.attr("r", smallestDim)
 						.remove(); 
 
 					d3.select(this).style('fill', 'orange');
 					d.data.circle = false;
 				}
 	    })
+
+	//text implimentations: svg, canvas, or foreignObject (html)
+	/*svg.append("text")
+		.attr("id", "valueOutput")
+		.attr("text-anchor", "middle")
+		.attr("transform","translate(" + innerRadius/20 + "," + (innerRadius/24 - 12) + ")");
+	d3.select('#valueOutput').html("Hover to View");
+	svg.append("text")
+		.attr("id", "valueSource")
+		.attr("text-anchor", "middle")
+		.attr("transform","translate(" + innerRadius/20 + "," + (innerRadius/24 + 12) + ")");
+	d3.select('#valueSource').html("Total Budget: " + formatNumber(d3.sum(budget)));*/
+
+	var valueText = {h: 100, w: 300};
+	svg.append("foreignObject") //Note this is not supported by all browsers
+		.attr("x", -valueText.w/2)
+		//.attr("y", -valueText.h/2)
+		.attr("width", valueText.w)
+		.attr("height", valueText.w)
+		.classed("valueText", true)
+		.append("xhtml:body").append("xhtml:p")
+		.html("<b>Hover To View </b><br/>" + "Total Budget: " + formatNumber(d3.sum(budget)));
 
 	d3.select('#chartArea').select('svg').append('circle')
   	.attr("r", innerRadius)
